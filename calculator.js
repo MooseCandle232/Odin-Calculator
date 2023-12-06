e8b02282ba14959419b9b0df7d004638dfaca271
Ian <73611215+MooseCandle232@users.noreply.github.com>
2023-12-06 18:09:45 -0500

    created calculator.js, index.html and styelsheet.css

---
+++ calculator.js
@@ -0,0 +1,23 @@
+let firstNum;
+let secondNum;
+let operator;
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operator, firstNum, secondNum) {
+    
+}
